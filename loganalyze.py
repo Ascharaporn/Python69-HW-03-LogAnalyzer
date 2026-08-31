de233f4ec8cbd709b00e7d0dc612bb79399d1d85
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2026-08-31 17:05:46 +0700

    Initial commit

---
+++ loganalyze.py
@@ -0,0 +1,13 @@
+def analyze_user_activity(log_file_path: str) -> dict:
+    #your code here
+    pass
+
+if __name__ == "__main__":
+    result = analyze_user_activity("activity.log")
+    from pprint import pprint
+    pprint(result)
+
+# {'action_counts': {'login': 2, 'logout': 2, 'submit': 1, 'view': 2},
+#  'average_session_time': 160.0,
+#  'most_active_user': 'u002',
+#  'total_users': 2}
